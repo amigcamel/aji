89b851c4fe7ba045cca18a09adb02a69dbaa60e7
achiii <amigcamel@gmail.com>
2017-01-19 17:32:53 +0800

    add whitenoise

--- aji/settings.py
+++ aji/settings.py
@@ -361,3 +361,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ""
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- aji/wsgi.py
+++ aji/wsgi.py
@@ -11,8 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
+from whitenoise.django import DjangoWhiteNoise
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("aji"))
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
