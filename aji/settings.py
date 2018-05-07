b616f19de07ddf1d105454e01b849fdc81943c67
Aji (阿吉) <amigcamel@gmail.com>
2018-05-07 11:44:29 +0800

    add path ./shared

--- aji/settings.py
+++ aji/settings.py
@@ -183,7 +183,7 @@ STATIC_URL = "/static_all/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static_all')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'shared/static_all')
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
